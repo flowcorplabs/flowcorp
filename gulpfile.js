ff9f3e3f79b8cd1d8d611b0193467347f5d3ac2b
Arthur Falcão <arthurfalcao77@gmail.com>
2019-02-05 17:03:49 -0300

    Changing project structure and add gulp

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+const { src, dest, parallel, watch } = require('gulp');
+const imagemin = require('gulp-imagemin');
+const browserSync = require('browser-sync');
+
+function buildImg() {
+
+    return src('src/assets/img/**/*')
+        .pipe(imagemin())
+        .pipe(dest('src/assets/img'));
+}
+
+function server() {
+
+    browserSync.init({
+
+        server: {
+            baseDir: 'src'
+        }
+    });
+
+    watch('src/**/*').on('change', browserSync.reload);
+}
+
+exports.buildImg = buildImg;
+exports.server = server;
+exports.default = parallel(server, buildImg);
